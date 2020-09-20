e0eca73aebdb6917d032e47954334a57004232a0
Mike <miked7531@gmail.com>
2020-09-20 14:53:08 -0400

    pg .183
    while/do while loops

---
+++ ConsoleappBool/ConsoleappBool/while.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleappBool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // while
+            int index = 1;
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            Console.ReadLine();
+            // do while
+            int index1 = 6;
+            do
+            {
+                Console.WriteLine(index1);
+                index1++;
+            } while (index <= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
