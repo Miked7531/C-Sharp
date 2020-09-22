1451aff054eb9ee6152f50f265175e2c54519d63
Mike <miked7531@gmail.com>
2020-09-22 14:21:29 -0400

    help pg. 186

---
+++ Concatenatepara/Concatenatepara/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concatenatepara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence of your choice: ");
+            string userInput = string.ToUpper(Console.ReadLine());
+
+            Console.WriteLine("Enter another sentence of your choice: ");
+            string userInput1 = Console.ReadLine();
+
+            Console.WriteLine("Enter another sentence of your choice: ");
+            string userInput2 = Console.ReadLine();
+
+            Console.WriteLine("Enter another sentence of your choice: ");
+            string userInput3 = Console.ReadLine();
+
+            userInput += " "; userInput1 += " "; userInput2 += " "; userInput3 += " ";
+            Console.ReadLine();
+           
+        }
+    }
+}
