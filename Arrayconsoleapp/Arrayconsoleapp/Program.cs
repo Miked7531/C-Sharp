40cde003957071d87849cc44e61d51a359b31375
Mike <miked7531@gmail.com>
2020-10-05 16:58:39 -0400

    pg.191

--- Arrayconsoleapp/Arrayconsoleapp/Program.cs
+++ Arrayconsoleapp/Arrayconsoleapp/Program.cs
@@ -10,6 +10,19 @@ namespace Arrayconsoleapp
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[5];
+            numbers[0] = 12;
+            numbers[1] = 3;
+            numbers[2] = 5;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
