973be2da44b3728e6260e34e967f01c644f0911f
Mike <miked7531@gmail.com>
2020-10-20 18:05:44 -0400

    prac

---
+++ Sixpartassignment/Sixpartassignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracdump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bus = { "yellow", "blue", "green", "pink" };
+            Console.WriteLine("Enter some text: ");
+            string userInput = Console.ReadLine();
+
+            foreach (string i in bus)
+            {
+                Console.Write(i.ToString() + " " + userInput + "\n");
+            }
+
+            Console.ReadLine();
+
+            int k = 0;
+            while (k < 5)
+            {
+                Console.WriteLine(k);
+                k++; //loops forever if i remove the incrementing k
+            }
+            Console.ReadLine();
+
+            for (int o = 0; o <= 10; o = o + 2)
+            {
+                Console.WriteLine(o);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sixinfiniteloop-fix/sixinfiniteloop-fix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sixinfiniteloop_fix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++; //loops forever if i remove the incrementing i
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
