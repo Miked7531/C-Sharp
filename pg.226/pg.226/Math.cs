1fdcaf8af1e11853d0d1b028a5ebae2f1c3b1eec
Mike <miked7531@gmail.com>
2020-11-22 16:43:02 -0500

    help

---
+++ pg.226/pg.226/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._226
+{
+    class Math
+    {
+        public int myAdd(int x)
+        {
+            return x + x;
+        }
+        public double myAdd(double y)
+        {
+            return y * y;
+        }
+        public int myAdd(string z)
+        {
+            return Convert.ToInt32("z - z");
+        }
+    }
+}
---
+++ pg.226/pg.226/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math numb = new Math();
+            Console.WriteLine("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a decimal: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter a string to convert to an integer: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            numb.myAdd(x);
+            numb.myAdd(y);
+            numb.myAdd(z);
+            Console.ReadLine();
+            
+        }
+    }
+}
