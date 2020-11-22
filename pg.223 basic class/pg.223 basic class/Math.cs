ed628e31a6266252daecd453186f46278af2b939
Mike <miked7531@gmail.com>
2020-11-22 14:26:26 -0500

    help

---
+++ pg.223 basic class/pg.223 basic class/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._223_basic_class
+{
+    public class Math
+    {
+
+        public int myAdd(int x)
+        {
+            return x + x;
+        }
+        public int mySub(int x)
+        {
+            return x - x;
+        }
+        public int myMulti(int x)
+        {
+            return x * x;
+        }
+
+        
+    }
+}
---
+++ pg.223 basic class/pg.223 basic class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._223_basic_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math numb = new Math();
+            Console.WriteLine("Let's do some basic math, please enter a number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            numb.myAdd(y);
+            numb.mySub(y);
+            numb.myMulti(y);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        
+    }
+}
