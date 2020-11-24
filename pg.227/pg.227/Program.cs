6dfe3817dca38fe89740aeb671705548c911631b
Mike <miked7531@gmail.com>
2020-11-24 14:36:07 -0500

    help

--- pg.227/pg.227/Program.cs
+++ pg.227/pg.227/Program.cs
@@ -10,6 +10,20 @@ namespace pg._227
     {
         static void Main(string[] args)
         {
+            twoTime math = new twoTime();
+
+            Console.WriteLine("Enter two numbers, but don't worry about the second one: ");
+            string[] userInput = Console.ReadLine().Split();
+
+            int x = Convert.ToInt32(userInput[0]);
+            int z = Convert.ToInt32(userInput[1]);
+
+            int result = math.myNumb(x, z);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+            
+
 
         }
     }
---
+++ pg.227/pg.227/twoTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._227
+{
+    class twoTime
+    {
+        public int myNumb(int x, int z, int y = 20)
+        {
+            return x + z + y;
+        
+
+        }
+    }
+}
