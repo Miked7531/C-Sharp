a3391ab375ce5e2a61fdc27e9daa13d6c5851a7d
Mike <miked7531@gmail.com>
2020-11-25 12:59:23 -0500

    help pg.228

---
+++ pg.228/pg.228/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._228
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            twoTegs royal = new twoTegs();
+
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            
+             
+
+            
+        }
+    }
+}
---
+++ pg.228/pg.228/twoTegs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace pg._228
+{
+    public class twoTegs
+    {
+        
+
+        static public void myOp(int x = 5, int y = 10)
+        {
+
+            if (x + 5 == y)
+            {
+                Console.WriteLine("You did it");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time");
+            }
+
+           
+            
+
+            
+
+        }
+    }
+}
