d6bbce82cfc99db04b741095f5694f8d59f5a065
Mike <miked7531@gmail.com>
2020-11-26 17:33:38 -0500

    maybe?
    not sure if this is correct or what

---
+++ pg.231/pg.231/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._231
+{
+    public static class myCitats
+    {
+        public static string bigstatic(string name)
+        {
+            name = "jon";
+            return name;
+        }
+        
+    }
+    
+
+    public static class Program
+    {
+        static public void Main(string[] args)
+        {
+            twoTime math = new twoTime();
+
+            Console.WriteLine("Enter a name, but it will always be jon: ");
+            string nametime = myCitats.bigstatic(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            math.myClazz(x: userInput);
+
+            math.myClazz(out int y, out int p, x: userInput);
+
+            Console.WriteLine("Add {0}", y);
+            Console.WriteLine("Add {0}", p);
+
+            Console.WriteLine(nametime);
+
+            Console.ReadLine();
+        }
+
+        
+        
+    }
+}
---
+++ pg.231/pg.231/twoTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._231
+{
+    class twoTime
+    {
+        public void myClazz(int x)
+        {
+            Console.WriteLine("Divided value is: " + x / 2);
+
+
+        }
+        
+        public void myClazz(out int p, out int y, int x)
+        {
+            p = 30;
+            y = 40;
+            p += p;
+            y += y + x;
+        }
+        
+    }
+}
