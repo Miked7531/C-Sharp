92023c3f0e54cb55dd06a0ba092d6cbc958e7266
Mike <miked7531@gmail.com>
2020-11-28 13:13:17 -0500

    maybe?

---
+++ pg.244/pg.244/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._244
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee staff = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            staff.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
