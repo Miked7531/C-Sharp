28d10c0de62d84323f231e8a6669049c1ea3611c
Mike <miked7531@gmail.com>
2020-12-05 13:40:56 -0500

    help lambda/generics

---
+++ pg.272 lambda/pg.272 lambda/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._272_lambda
+{
+    public class Employee
+    {
+        private string firstName;
+        private string lastName;
+
+        public Employee(string firstName, string lastName, int Id)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            ID = Id;
+        }
+
+        public string Name
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string Name1
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public int ID { get; set; }
+
+    }
+}
---
+++ pg.272 lambda/pg.272 lambda/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._272_lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("Joe", "Blow", 1));
+            employees.Add(new Employee("Joe", "Doe",2));
+            employees.Add(new Employee("Jane", "Doe",3));
+            employees.Add(new Employee("Beth", "Blow",4));
+            employees.Add(new Employee("Janice", "Smith",5));
+            employees.Add(new Employee("Mr", "Smith",6));
+            employees.Add(new Employee("Mr", "Anderson",7));
+            employees.Add(new Employee("Alex", "Lion",8));
+            employees.Add(new Employee("Sophia", "King",9));
+            employees.Add(new Employee("Ding", "Dong",10));
+
+            //foreach loop making a joe list
+            var result = from x in employees
+                         where x.Name == "Joe"
+                         select x;
+            foreach (var employee in result)
+            {
+                Console.WriteLine(employee.ID + ", " + employee.Name + ", " + employee.Name1);
+            }
+
+
+            //List<Employee> newList = employees.(y => y.Name == );
+
+            var newNumb = employees.Where(z => z.ID > 5).ToList();
+
+            Console.WriteLine(string.Join(System.Environment.NewLine, newNumb));
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
