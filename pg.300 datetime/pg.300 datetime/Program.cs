e15704bd7e76c9188d2fd976571c8a658b087f1a
Mike <miked7531@gmail.com>
2021-01-13 11:48:55 -0500

    pg. 300
    datetime userinput

---
+++ pg.300 datetime/pg.300 datetime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg._300_datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            DateTime currentTime = DateTime.Now;
+            DateTime userInput = currentTime.AddHours(x);
+            Console.WriteLine(string.Format("{0} {1}", currentTime, userInput));
+
+            Console.ReadLine();
+        }
+    }
+}
