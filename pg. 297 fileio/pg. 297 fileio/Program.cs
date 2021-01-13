dd3a5a32bb77acb562b961a03404c4bbf7d3a85f
Mike <miked7531@gmail.com>
2021-01-13 10:47:08 -0500

    pg.297
    file io

---
+++ pg. 297 fileio/pg. 297 fileio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pg._297_fileio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\miked\Logs\log.txt";
+
+            Console.WriteLine("Enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //string[] lines = File.ReadAllLines(filePath); 
+
+            List<string> lines = new List<string>();
+            lines.Add(input.ToString());
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            
+            File.WriteAllLines(filePath, lines);
+
+            Console.ReadLine();
+        }
+    }
+}
