b2de15739d93a46aae8ccb5240ca8eb083cd340e
Mike <miked7531@gmail.com>
2021-04-01 23:40:08 -0400

    SQL/C# projects
    more of a real life project based around a work environment using C# with SQL.

---
+++ CourseReportEmailer/CourseReportEmailer/Program.cs
@@ -0,0 +1,36 @@
+﻿using CourseReportEmailer.Models;
+using CourseReportEmailer.Repository;
+using CourseReportEmailer.Workers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CourseReportEmailer
+{
+    class Program
+    {
+        private static object enrollmentSheetCreator;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                EnrollmentDetailReportCommand command = new EnrollmentDetailReportCommand(@"Data Source=localhost;Initial Catalog=CourseReport;Integrated Security=True");
+                IList<EnrollmentDetailReportModel> models = command.GetList();
+
+                var reportFileName = "EnrollmentDetailsReport.xlsx";
+                EnrollmentDetailReportSpreadSheetCreator enrollmentSheetCreator = new EnrollmentDetailReportSpreadSheetCreator();
+                enrollmentSheetCreator.Create(reportFileName, models);
+
+                EnrollmentDetailReportEmailSender emailer = new EnrollmentDetailReportEmailSender();
+                emailer.Send(reportFileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: {0}", ex.Message);
+                
+            }
+        }
+    }
+}
